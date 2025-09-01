e7296d3083c9d7958e1aac4167205c2cffa6984a
Pranesh <praneshmadhan646@gmail.com>
2025-09-01 14:07:41 +0530

    changed file name in reademe_generator

--- backend/readme_generator.py
+++ backend/readme_generator.py
@@ -1,5 +1,5 @@
 def generate_readme(repo_info, content, license_type=None):
-    with open("README.md", "w", encoding="utf-8") as f:
+    with open("readme.md", "w", encoding="utf-8") as f:
         f.write(f"# {repo_info['name']}\n\n")
         f.write(content)
         if license_type:
