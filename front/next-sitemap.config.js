eabf9bebf41ba745130ec0e317700753fab91050
Pranesh <praneshmadhan646@gmail.com>
2025-09-07 19:13:11 +0530

    Added next-sitemap package for SEO optimization

---
+++ front/next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type
+{import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://readmegeneratorgithub.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 5000,
+} // (optional)
